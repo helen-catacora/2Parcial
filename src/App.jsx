b7177807eefb8d9ebcd263544b81cb1aab0c9a4b
Helen Catacora <catacorapatanahelenjazmin@gmail.com>
2023-10-19 11:18:30 -0400

    segundo componente

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Perfil from './componentes/Perfil'
+import Portada from './componentes/Portada'
 
 function App() {
 
   return (
     <>
-      <Perfil />
+      <Portada />
     </>
   )
 }
---
+++ src/componentes/Portada.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Portada() {
+  return (
+    <div>
+        <img src="/Banner _DDD.png" alt="" />
+    </div>
+  )
+}
+
+export default Portada
