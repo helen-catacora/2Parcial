3df8fa0ef986547e48097c6427c577295a7d39e6
Helen Catacora <catacorapatanahelenjazmin@gmail.com>
2023-10-19 11:21:00 -0400

    composicion de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Perfil from './componentes/Perfil'
-import Portada from './componentes/Portada'
+import Card from './componentes/Card'
 
 function App() {
 
   return (
     <>
-      <Portada />
+      <Card />
     </>
   )
 }
---
+++ src/componentes/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Portada from './Portada'
+import Perfil from './Perfil'
+
+function Card() {
+  return (
+    <div>
+        <Portada />
+        <Perfil />
+    </div>
+  )
+}
+
+export default Card
